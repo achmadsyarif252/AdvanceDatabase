351c68245072ce792567c03de364f76731f1ed80
achmad2002 <19kaizen@gmail.com>
2023-01-10 03:32:56 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
@@ -0,0 +1,25 @@
+package com.dicoding.mystudentdata
+
+import androidx.lifecycle.LiveData
+import com.dicoding.mystudentdata.database.*
+import com.dicoding.mystudentdata.helper.InitialDataSource
+
+class StudentRepository(private val studentDao: StudentDao) {
+    fun getAllStudent(): LiveData<List<Student>> = studentDao.getAllStudent()
+    fun getAlStudentAndUniversity(): LiveData<List<StudentAndUniversity>> =
+        studentDao.getAllStudentAndUniversity()
+
+    fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> =
+        studentDao.getAllUniversityAndStudent()
+
+    fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>> =
+        studentDao.getAllStudentWithCourse()
+
+    suspend fun insertAllData() {
+        studentDao.insertStudent(InitialDataSource.getStudents())
+        studentDao.insertUniversity(InitialDataSource.getUniversities())
+        studentDao.insertCourse(InitialDataSource.getCourses())
+        studentDao.insertCourseStudentCrossRef(InitialDataSource.getCourseStudentRelation())
+
+    }
+}
